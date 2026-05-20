f0cdce3d2d18c78e92600d0edf3b643535141d63
jmossVB <98797411+jmossVB@users.noreply.github.com>
2026-05-20 16:54:04 -0500

    Update app.py

--- app.py
+++ app.py
@@ -5,12 +5,12 @@ st.set_page_config(
     page_icon="🖥️",
     layout="wide",
 )
+
 st.sidebar.title("Secciones")
+st.title("PROYECTO 1 – APLICACIÓN EN STREAMLIT")
+st.write("Elaborado por Jose Alex Mosquera Amaro")
+
 pagina = st.sidebar.selectbox(
     "📂 Navegación",
     ["🏠 Home", "📊 Ejercicio 1", "🧮 Ejercicio 2", "⚙️ Ejercicio 3", "🗄️ Ejercicio 4"],
 )
-
-st.title("PROYECTO 1 – APLICACIÓN EN STREAMLIT")
-
-st.write("Elaborado por Jose Alex Mosquera Amaro")
