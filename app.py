82128633def86fd7f49fd6dab116f965a191f99d
jmossVB <98797411+jmossVB@users.noreply.github.com>
2026-05-20 16:38:53 -0500

    Update app.py

--- app.py
+++ app.py
@@ -1,5 +1,10 @@
 import streamlit as st
 
+st.set_page_config(
+    page_title="Proyecto 1 – Aplicación en Streamlit",
+    page_icon="🖥️",
+    layout="wide",
+)
 st.title("Mi primera aplicacion en python")
 st.sidebar.title("Parametros")
 st.write("Elaborado por jmoss")
